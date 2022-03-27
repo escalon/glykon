0c873269179e63e421b1946ca3adc7d9a96da6ed
Dietrich Schulten <ds@escalon.de>
2022-03-27 15:06:50 +0200

    Transform XDatenfelder input to form definition file

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// Include or require PO.js so that PO variable is available
-// For instance, in node.js:
 var xdf = require('./mappings/xdf').xdf;
 var Jsonix = require('jsonix').Jsonix
- 
-// First we construct a Jsonix context - a factory for unmarshaller (parser)
-// and marshaller (serializer)
+var jsonata = require("jsonata");
+const fs = require('fs')
+
 var context = new Jsonix.Context([xdf]);
- 
-// Then we create a unmarshaller
 var unmarshaller = context.createUnmarshaller();
- 
-// Unmarshal an object from the XML retrieved from the URL
+
+// xdatenfelder to JSON
 unmarshaller.unmarshalFile('bewohnerparken/S05000058V1.0_xdf2.xml',
-    // This callback function will be provided
-    // with the result of the unmarshalling
     function (unmarshalled) {
-        // Alice Smith
-        console.log(JSON.stringify(unmarshalled.value, null, 2));
-        // Baby Monitor
-        // console.log(unmarshalled.value.items.item[1].productName);
+        try {
+            const jsonataTemplate = fs.readFileSync('jsonata/example-formtemplate.jsonata', 'utf8')
+            const jsonataExpr = jsonata(jsonataTemplate);
+            const formJson = jsonataExpr.evaluate(unmarshalled.value);
+            console.log(JSON.stringify(formJson, null, 2))
+        } catch (err) {
+            console.error(err)
+        }
     });
